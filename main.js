715a8178dd558e72bd7c875c85befeb9dfd64074
himangshu06 <76275772+himangshu06@users.noreply.github.com>
2021-06-16 20:23:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var synth = window.speechSynthesis;
+     speak_data2 = "Welcome Again your webpage has been Loaded Sucessfully! This webapge has been made by incredible Studios also kown as Himangshu! Have a Great day!"
+     
+     var utterThis = new SpeechSynthesisUtterance(speak_data2);
+     synth.speak(utterThis);
+
+
+
